a215c80c8e3bec4cef3ce4fbed07400228427203
Tyler Taylor <61921198+UnstablePayload@users.noreply.github.com>
2025-05-21 22:23:59 -0400

    Updated Wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,20 +8,13 @@ local config = wezterm.config_builder()
 config.initial_cols = 120
 config.initial_rows = 40
 
-<<<<<<< HEAD
-config.font_size = 16
 config.color_scheme = 'shades-of-purple'
 
-config.window_background_opacity = .8
-=======
 font = wezterm.font('JetBrains Mono Nerd Font')
 config.font_size = 18
 
-config.color_scheme = 'shades-of-purple'
-
 config.window_background_opacity = .8
 config.macos_window_background_blur = 20
->>>>>>> 883ec5d (config updates and snippets.lua)
 
 -- Returns the config
 return config
