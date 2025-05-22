f2cb1373c8ad17aa384673692fadde1addc27fc3
Tyler Taylor <61921198+UnstablePayload@users.noreply.github.com>
2025-05-22 10:43:37 -0400

    theme update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,13 +8,13 @@ local config = wezterm.config_builder()
 config.initial_cols = 120
 config.initial_rows = 40
 
-config.color_scheme = 'shades-of-purple'
-
+config.color_scheme = 'kanagawabones'
 font = wezterm.font('JetBrains Mono Nerd Font')
 config.font_size = 18
 
 config.window_background_opacity = .8
 config.macos_window_background_blur = 20
-
+-- config.tab_bar_at_bottom = truei
+config.enable_tab_bar = false
 -- Returns the config
 return config
