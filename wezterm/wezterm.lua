f9b8087b876de445d4161b9564fe4e009d0e6947
Tyler Taylor <61921198+UnstablePayload@users.noreply.github.com>
2025-05-31 18:31:03 -0400

    Updated

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,7 +14,7 @@ config.font_size = 15
 
 config.window_background_opacity = .8
 config.macos_window_background_blur = 20
--- config.tab_bar_at_bottom = truei
-config.enable_tab_bar = false
+config.tab_bar_at_bottom = true
+config.enable_tab_bar = ture
 -- Returns the config
 return config
